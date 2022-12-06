0999c98ccffcddb7a1723c974fa9302b4e17d8f8
Rodo G <74681384+Rodogg-20@users.noreply.github.com>
2022-12-06 10:20:47 -0600

    Update 2_generate_csvs.sh

--- samuel/el bueno/2_generate_csvs.sh
+++ samuel/el bueno/2_generate_csvs.sh
@@ -2,5 +2,4 @@
 
 mongoexport --db=shapi --collection=superheroes --type=csv --out superheroes_monetdb.csv --fields=name,slug,powerstats.intelligence,powerstats.strength,powerstats.speed,powerstats.durability,powerstats.power,powerstats.combat,appearance.height,appearance.weight,appearance.eyeColor,appearance.hairColor,appearance.gender,appearance.race,work.occupation,biography.fullName,biography.alterEgos,biography.firstAppearance,biography.aliases
 
-mongoexport --db=shapi --collection=superheroes --type=csv --out superheroes_n4j.csv --fields=name,slug,
-appearance.gender,appearance.race,biography.publisher,biography.placeOfBirth,biography.alignment,work.base,connections.groupAffiliation, connections.relatives
+mongoexport --db=shapi --collection=superheroes --type=csv --out superheroes_n4j.csv --fields=name,slug,appearance.gender,appearance.race,biography.publisher,biography.placeOfBirth,biography.alignment,work.base,connections.groupAffiliation, connections.relatives
